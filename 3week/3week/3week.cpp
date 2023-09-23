4ef0eef8d6b5bb31578931185b52acce101b0e22
panut20 <mukk1110@naver.com>
2023-09-23 23:32:29 +0900

    Update 3week.cpp

--- 3week/3week/3week.cpp
+++ 3week/3week/3week.cpp
@@ -27,6 +27,7 @@ int main()
         
         
         ch = _getch();
+        std::chrono::system_clock::time_point start = std::chrono::system_clock::now(); // 입력과 동시에 시간 체크
 //      printf("%c", ch);
 
 //      char는 1바이트, 방향키는 아스키 코드가 2개있는 특수기호. (224, ~~~) 즉, 224를 char자료형에 받으면 1 1 1 0 0 0 0 0 라는 비트가 저장되고,
@@ -37,12 +38,14 @@ int main()
                 ch = _getch();
                 if (ch == 'a') {
                     ch = _getch();
-                    if (ch == 's') {
-                        printf("aaaaaaaaaaaa");
+                    std::chrono::duration<double>sec = std::chrono::system_clock::now() - start; // 완성 직전에 시간체크 및 0.3초 체크
+                    if (ch == 's' && sec.count() <= 0.3) {
+                        printf("아따따따뚜겐~\n");
                     }
                 }
 
             }
+
             if (ch == DOWN) {
                 ch = _getch();
                 printf("22222");
