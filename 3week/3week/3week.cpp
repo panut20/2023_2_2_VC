ae99757c3bb6bab1d138e99eb9cfbccb555560d1
panut20 <mukk1110@naver.com>
2023-09-23 23:59:04 +0900

    Update 3week.cpp

--- 3week/3week/3week.cpp
+++ 3week/3week/3week.cpp
@@ -1,7 +1,7 @@
 ﻿#include <iostream>
 #include<stdio.h>
 #include<conio.h>
-#include <chrono>
+#include <chrono>  // 시간작업관련 c 런타임 time보다 정확.
 
 #define ESC 27
 #define UP 72
@@ -11,27 +11,18 @@
 
 int main()
 {
-    /*
-    std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
-    int sum = 0;
-    for (int i = 0; i < 999999999; i++)
-        sum += i;
-    std::chrono::duration<double>sec = std::chrono::system_clock::now() - start;
-    std::cout << "for문을 돌리는데 걸리는 시간(초) : " << sec.count() << "seconds" << std::endl;
-    */
 
     char ch= ' ';
 
     printf("↑+ a + s = 아따따뚜겐~\n");
     printf("→+→ = 앞대쉬\n");
     printf("←+← = 백대쉬\n");
+    printf("도발\n");
 
     while (ch != ESC) {
         
-        
         ch = _getch();
         std::chrono::system_clock::time_point start = std::chrono::system_clock::now(); // 입력과 동시에 시간 체크
-//      printf("%c", ch);
 
 //      char는 1바이트, 방향키는 아스키 코드가 2개있는 특수기호. (224, ~~~) 즉, 224를 char자료형에 받으면 1 1 1 0 0 0 0 0 라는 비트가 저장되고,
 //      char는 signed라서 2의 보수체계에 따라 저장된 비트를 해석. 즉 -32가 됨. (오버플로우의 영향)
@@ -43,15 +34,11 @@ int main()
                     ch = _getch();
                     std::chrono::duration<double>sec = std::chrono::system_clock::now() - start; // 완성 직전에 시간체크 및 0.3초 체크
                     if (ch == 's' && sec.count() <= 0.3) {
-                        printf("아따따따뚜겐~\n");
+                        printf("아따따뚜겐~\n");
                     }
                 }
-
             }
 
-            if (ch == DOWN) {
-
-            }
             if (ch == LEFT) {
                 ch = _getch();
                 ch = _getch();
@@ -68,11 +55,25 @@ int main()
                 if (ch == RIGHT && sec.count() <= 0.3) {
                     printf("앞대쉬\n");
                 }
-                
             }
         }
-        if (ch == 'a') {
-            printf("asdd\n");
+
+
+        if (ch == 'e') {
+            ch = _getch();
+            if (ch == 'h') {
+                ch = _getch();
+                if (ch == 'q') {
+                    ch = _getch();
+                    if (ch == 'k') {
+                        ch = _getch();
+                        std::chrono::duration<double>sec = std::chrono::system_clock::now() - start;
+                        if (ch == 'f' && sec.count() <= 0.3) {
+                            printf("그렇게 하는거 아닌데~~ ~.~\n");
+                        }
+                    }
+                }
+            }
         }
 
     }
