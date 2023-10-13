5d58ce2eb7451483c15535cda36718c599da2ec0
panut20 <mukk1110@naver.com>
2023-10-14 04:19:22 +0900

    Update 5week.cpp
    아니 알아서 그려주고 사각형안에 범위도 인식해주네??????? 이걸 왜 몰랐지 없는게 없네...ㅋㅋㅋㅋㅋㅋ

--- 5week/5week/5week.cpp
+++ 5week/5week/5week.cpp
@@ -1,5 +1,5 @@
 ﻿#ifdef UNICODE
-#pragma comment(linker, "/entry:wWinMainCRTStartup /subsystem:console")
+#pragma comment(linker, "/entry:wWinMainCRTStartup /subsystem:windows")
 #else
 #pragma comment(linker, "/entry:WinMainCRTStartup /subsystem:console")
 #endif
@@ -11,8 +11,8 @@ POINT startPoint = { 0 };
 POINT endPoint = { 0 };
 int isMouseLButtonPressed = 0;
 int isMouseRButtonPressed = 0;
-RECT rect = { 0 }; // 현재 그려진 사각형의 위치와 크기를 저장합니다.
-RECT savedRect = { 0 }; // 이동하기 전의 사각형의 위치와 크기를 저장합니다.
+RECT rect = { 0 };
+
 
 // 윈도우의 이벤트를 처리하는 콜백(Callback) 함수.
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
@@ -21,16 +21,11 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 	case WM_LBUTTONDOWN:
 	{
-		HDC hdc = GetDC(hwnd);
-
-		// 기존에 그려진 사각형을 지우기 위해 화면을 흰색으로 칠함.
-		RECT clientRect;
-		GetClientRect(hwnd, &clientRect);
-		FillRect(hdc, &clientRect, (HBRUSH)(COLOR_WINDOW + 1));
-
 		startPoint.x = LOWORD(lParam);
 		startPoint.y = HIWORD(lParam);
 		isMouseLButtonPressed = 1;
+		rect = { 0 };
+		InvalidateRect(hwnd, NULL, TRUE);
 	}
 	break;
 
@@ -42,56 +37,45 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		{
 			endPoint.x = LOWORD(lParam);
 			endPoint.y = HIWORD(lParam);
-
-			HDC hdc = GetDC(hwnd);
-			RECT rect = { startPoint.x, startPoint.y, endPoint.x, endPoint.y };
-			HBRUSH hBrush = CreateSolidBrush(RGB(255, 0, 255)); // 핑크 브러시 생성
-
-			if (hBrush == NULL)
-			{
-				MessageBox(NULL, L"CreateSolidBrush failed!", L"Error", MB_ICONERROR);
-				exit(-1);
-			}
-
-			FillRect(hdc, &rect, hBrush);
-			ReleaseDC(hwnd, hdc);
-
+			rect.left = min(startPoint.x, endPoint.x);
+			rect.top = min(startPoint.y, endPoint.y);
+			rect.right = max(startPoint.x, endPoint.x);
+			rect.bottom = max(startPoint.y, endPoint.y);
+			InvalidateRect(hwnd, NULL, TRUE);
 		}
 
+		if (isMouseRButtonPressed && !IsRectEmpty(&rect)) {
+			rect.left += LOWORD(lParam) - startPoint.x;
+			rect.top += HIWORD(lParam) - startPoint.y;
+			rect.right += LOWORD(lParam) - startPoint.x;
+			rect.bottom += HIWORD(lParam) - startPoint.y;
+			startPoint.x = LOWORD(lParam);
+			startPoint.y = HIWORD(lParam);
+			InvalidateRect(hwnd, NULL, TRUE);
+		}
 	}
 	break;
-
 	case WM_LBUTTONUP:
 	{
 		endPoint.x = LOWORD(lParam);
 		endPoint.y = HIWORD(lParam);
-
 		isMouseLButtonPressed = 0;
-
-		//그림을 그리는 코드
-		HDC hdc = GetDC(hwnd);
-		RECT rect = { startPoint.x, startPoint.y, endPoint.x, endPoint.y };
-		HBRUSH hBrush = CreateSolidBrush(RGB(255, 0, 255)); // 핑크 브러시 생성
-
-		if (hBrush == NULL)
-		{
-			MessageBox(NULL, L"CreateSolidBrush failed!", L"Error", MB_ICONERROR);
-			exit(-1);
-		}
-
-		FillRect(hdc, &rect, hBrush);
-		ReleaseDC(hwnd, hdc);
+		InvalidateRect(hwnd, NULL, TRUE);
 	}
+	break;
 
 	case WM_RBUTTONDOWN:
 	{
-		startPoint.x = LOWORD(lParam);
-		startPoint.y = HIWORD(lParam);
-		isMouseRButtonPressed = 1;
+		POINT mousePos;
+		mousePos.x = LOWORD(lParam);
+		mousePos.y = HIWORD(lParam);
 
-		// 마우스 우클릭 드래그로 이동할 때, 현재 사각형의 위치를 저장합니다.
-		savedRect = rect;
-
+		if (PtInRect(&rect, mousePos)) {
+			startPoint.x = LOWORD(lParam);
+			startPoint.y = HIWORD(lParam);
+			isMouseRButtonPressed = 1;
+		}
+		InvalidateRect(hwnd, NULL, TRUE);
 	}
 	break;
 
@@ -100,43 +84,24 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		endPoint.x = LOWORD(lParam);
 		endPoint.y = HIWORD(lParam);
 		isMouseRButtonPressed = 0;
-
-		// 이동한 거리를 계산하여 사각형의 위치를 조정합니다.
-		int deltaX = endPoint.x - startPoint.x;
-		int deltaY = endPoint.y - startPoint.y;
-		rect.left = savedRect.left + deltaX;
-		rect.right = savedRect.right + deltaX;
-		rect.top = savedRect.top + deltaY;
-		rect.bottom = savedRect.bottom + deltaY;
-
-		HDC hdc = GetDC(hwnd);
-		rect = { rect.left, rect.top, rect.right, rect.bottom };
-		HBRUSH hBrush = CreateSolidBrush(RGB(255, 0, 255)); // 핑크 브러시 생성
-
-		if (hBrush == NULL)
-		{
-			MessageBox(NULL, L"CreateSolidBrush failed!", L"Error", MB_ICONERROR);
-			exit(-1);
-		}
-
-		FillRect(hdc, &rect, hBrush);
-		ReleaseDC(hwnd, hdc);
+		InvalidateRect(hwnd, NULL, TRUE);
 	}
 	break;
 
 	case WM_PAINT:
 	{
-		HDC hdc = GetDC(hwnd);
-			
-		if (isMouseLButtonPressed)
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hwnd, &ps);
+		FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+
+		if (!IsRectEmpty(&rect))
 		{
-
-			RECT rect = { startPoint.x, startPoint.y, endPoint.x, endPoint.y };
-			FillRect(hdc, &rect, (HBRUSH)(COLOR_WINDOW + 1));
-
+			HBRUSH hBrush = CreateSolidBrush(RGB(255, 0, 255));
+			FillRect(hdc, &rect, hBrush);
+			DeleteObject(hBrush);
 		}
 
-		ReleaseDC(hwnd, hdc);
+		EndPaint(hwnd, &ps);
 
 	}
 	break;
@@ -150,6 +115,7 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
 	return S_OK;
 }
+
 #ifdef UNICODE
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd)
 #else
@@ -213,7 +179,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	while (msg.message != WM_QUIT)
 	{
 		// 메시지 처리.
-		if (GetMessage(&msg, hwnd, 0, 0))
+		if (GetMessage(&msg, NULL, 0, 0))
 			//if (PeekMessage(&msg, hwnd, 0, 0, PM_REMOVE)) // PM_REMOVE의 자리는 이 메세지를 쓰고 어떡할거냐.의 의미인데 지운다는 것임.
 		{
 
